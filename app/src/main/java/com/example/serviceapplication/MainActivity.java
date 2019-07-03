f1112018402f7724fe9f560209535f185e110d55
benedict.b.contawe <benedict.b.contawe@accenture.com>
2019-07-03 19:35:19 +0800

    Creating new class for internet connection check

--- app/src/main/java/com/example/serviceapplication/MainActivity.java
+++ app/src/main/java/com/example/serviceapplication/MainActivity.java
@@ -1,22 +1,11 @@
 package com.example.serviceapplication;
 
-import android.Manifest;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
     private TextView txtData;
@@ -27,38 +16,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         txtData = findViewById(R.id.txtData);
-
-
     }
 
     @Override
     protected void onStart() {
         super.onStart();
 
-        if (hasInternet()){
+
+
+        if (InternetConnection.hasInternet((ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE))){
             txtData.setText("Network Connection is available");
         }
-        else if (!hasInternet()){
+        else if (!(InternetConnection.hasInternet((ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE)))){
             txtData.setText("Network Connection is not available");
         }
 
     }
 
-    private boolean hasInternet(){
-    boolean hasWifi = false;
-    boolean hasMobileData = false;
 
-    ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
-    NetworkInfo[] networkInfos = connectivityManager.getAllNetworkInfo();
 
-        for (NetworkInfo info : networkInfos){
-        if (info.getTypeName().equalsIgnoreCase("WIFI"))
-            if (info.isConnected())
-                hasWifi = true;
-        if (info.getTypeName().equalsIgnoreCase("MOBILE"))
-            if (info.isConnected())
-                hasMobileData = true;
-        }
-        return hasMobileData||hasWifi;
-    }
 }
