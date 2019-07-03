6e14b21ebf79efafba2be9de013e4436966a9179
benedict.b.contawe <benedict.b.contawe@accenture.com>
2019-07-03 19:05:03 +0800

    Update for internet checker

---
+++ app/src/main/java/com/example/serviceapplication/CustomService.java
@@ -0,0 +1,17 @@
+package com.example.serviceapplication;
+
+import android.app.IntentService;
+import android.app.Notification;
+import android.content.Intent;
+import android.os.Build;
+import android.os.PowerManager;
+import android.os.SystemClock;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+
+public class CustomService {
+
+
+}
--- app/src/main/java/com/example/serviceapplication/MainActivity.java
+++ app/src/main/java/com/example/serviceapplication/MainActivity.java
@@ -1,30 +1,64 @@
 package com.example.serviceapplication;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText editTextInput;
+    private TextView txtData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editTextInput = findViewById(R.id.edit_text_input);
+        txtData = findViewById(R.id.txtData);
+
+
     }
 
-    public void startService(View v) {
-        String input = editTextInput.getText().toString();
+    @Override
+    protected void onStart() {
+        super.onStart();
 
-        Intent serviceIntent = new Intent(this, CustomIntentService.class);
-        serviceIntent.putExtra("inputExtra", input);
+        if (hasInternet()){
+            txtData.setText("Network Connection is available");
+        }
+        else if (!hasInternet()){
+            txtData.setText("Network Connection is not available");
+        }
 
-        ContextCompat.startForegroundService(this, serviceIntent);
+    }
+
+    private boolean hasInternet(){
+    boolean hasWifi = false;
+    boolean hasMobileData = false;
+
+    ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+    NetworkInfo[] networkInfos = connectivityManager.getAllNetworkInfo();
+
+        for (NetworkInfo info : networkInfos){
+        if (info.getTypeName().equalsIgnoreCase("WIFI"))
+            if (info.isConnected())
+                hasWifi = true;
+        if (info.getTypeName().equalsIgnoreCase("MOBILE"))
+            if (info.isConnected())
+                hasMobileData = true;
+        }
+        return hasMobileData||hasWifi;
     }
 }
